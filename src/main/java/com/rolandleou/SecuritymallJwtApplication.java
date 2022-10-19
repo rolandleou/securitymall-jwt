b30281a15205b2c94069799c97a4d123794ac086
Roland Liu <rolandleou@gmail.com>
2022-10-20 07:41:56 +0800

    Initial Spring security project with JWT

---
+++ src/main/java/com/rolandleou/SecuritymallJwtApplication.java
@@ -0,0 +1,13 @@
+package com.rolandleou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritymallJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritymallJwtApplication.class, args);
+	}
+
+}
